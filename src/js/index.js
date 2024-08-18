7a14ada651a006af2bd2615661ebc080f9969123
Juliane Granusso <jugranussoc@gmail.com>
2024-08-17 21:27:54 -0300

    adjusting index imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
 import "../styles/index.css";
 import {} from "./card-form.js";
 import {} from "./form.js";
-import {} from "./PopupWithImage.js";
-import {} from "./Card.js";
-import {} from "./FormValidator.js";
-import {} from "./Popup.js";
